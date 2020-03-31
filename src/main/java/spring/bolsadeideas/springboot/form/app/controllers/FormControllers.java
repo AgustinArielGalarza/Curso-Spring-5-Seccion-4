e19ef96ada77199a20494749ed39c1f1894f501d
AgustinArielGalarza <agustin.galarza96@gmail.com>
2020-03-31 16:31:26 -0300

    Validacion y expreciones regulares

--- src/main/java/spring/bolsadeideas/springboot/form/app/controllers/FormControllers.java
+++ src/main/java/spring/bolsadeideas/springboot/form/app/controllers/FormControllers.java
@@ -1,8 +1,5 @@
 package spring.bolsadeideas.springboot.form.app.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
@@ -10,35 +7,47 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import spring.bolsadeideas.springboot.form.app.model.domain.Usuario;
 
 @Controller
+@SessionAttributes("usuario") //se modifica la clase paraque pueda paasar al formulario atributos/propiedades que no hayan sido pasadas al form, ej:id. Del otro caso se pasará como null.
 public class FormControllers {
 	
 	@GetMapping("/form")
 	public String form(Model model) {
 		Usuario usuario = new Usuario();
+		usuario.setNombre("Agustin");
+		usuario.setApellido("Galarza");
+		usuario.setIdentificador("70.808.014-k");
 		model.addAttribute("titulo", "Form Usuario");
 		model.addAttribute("usuario",usuario);
 		return "form";
 	}
 	
 	@PostMapping("/form")
-	public String procesar(@Valid Usuario usuario , BindingResult result, Model model) {
+	public String procesar(@Valid Usuario usuario , BindingResult result, Model model, SessionStatus status) {
 		
 		model.addAttribute("titulo", "Resultado del Form");
 		
 		if (result.hasErrors()) {
-			Map<String, String> errores = new HashMap<>();
-			result.getFieldErrors().forEach(err ->{
-				errores.put(err.getField(),"El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
-			});
-			model.addAttribute("error",errores);
+//			1- manejo de errores de forma manual, se encuentra en el punto de abajo
 			return "form";
 		}
 		
 		model.addAttribute("usuario", usuario);
+		status.setComplete();
 		return "resultado";
 	}
 }
+
+
+//1.
+//Map<String, String> errores = new HashMap<>();
+//result.getFieldErrors().forEach(err ->{
+//	errores.put(err.getField(),"El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
+//});
+//model.addAttribute("error",errores);
--- src/main/java/spring/bolsadeideas/springboot/form/app/model/domain/Usuario.java
+++ src/main/java/spring/bolsadeideas/springboot/form/app/model/domain/Usuario.java
@@ -1,19 +1,53 @@
 package spring.bolsadeideas.springboot.form.app.model.domain;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 public class Usuario {
 	
-	@NotEmpty
+	@NotEmpty(message = "el nombre no puede estar vacio")
+	private String nombre;
+	@NotEmpty()
+	private String apellido;
+	@Pattern(regexp = "[0-9]{2}[.][0-9]{3}[.][0-9]{3}[-][aA-zZ]{1}")
+	private String identificador;
+	@NotEmpty()
+	@Size(min = 3, max = 8)
 	private String username;
-	@NotEmpty
+	@NotEmpty()
 	private String password;
-	@NotEmpty
+	@NotEmpty(message = "el email no puede estar vacio")
+	@Email
 	private String email;
 	
 	public Usuario() {
 	}
 	
+	
+	public String getIdentificador() {
+		return identificador;
+	}
+
+
+	public void setIdentificador(String identificador) {
+		this.identificador = identificador;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
 	public String getUsername() {
 		return username;
 	}
